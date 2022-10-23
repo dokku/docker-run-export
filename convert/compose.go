6d0aca44b4246ba05c764258385eea03c676b8df
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2022-10-23 02:10:58 -0400

    feat: implement a few more flags

--- convert/compose.go
+++ convert/compose.go
@@ -382,6 +382,55 @@ func ToCompose(projectName string, c *arguments.Args, arguments map[string]comma
 
 	service.ContainerName = c.ContainerName
 
+	if len(c.Network) > 0 {
+		project.Networks = map[string]types.NetworkConfig{
+			"default": {
+				Name:     c.Network,
+				External: types.External{External: true},
+			},
+		}
+	}
+
+	if len(c.NetworkAlias) > 0 {
+		if len(service.Networks) == 0 {
+			service.Networks = map[string]*types.ServiceNetworkConfig{
+				"default": {},
+			}
+		}
+
+		service.Networks["default"].Aliases = c.NetworkAlias
+	}
+
+	if c.NoHealthcheck {
+		if service.BlkioConfig == nil {
+			service.HealthCheck = &types.HealthCheckConfig{}
+		}
+
+		service.HealthCheck.Disable = true
+	}
+
+	if len(c.Mount) > 0 {
+		warnings = multierror.Append(warnings, fmt.Errorf("unable to set --mount property in compose spec as the mounts must be validated and parsed"))
+	}
+
+	if c.OomKillDisable {
+		warnings = multierror.Append(warnings, fmt.Errorf("unable to set --oom-kill-disable property in compose spec as the property is not valid in compose v3"))
+	}
+
+	if c.OomScore != 0 {
+		warnings = multierror.Append(warnings, fmt.Errorf("unable to set --oom-score property in compose spec as the property is not valid in compose v3"))
+	}
+	if len(c.Pid) > 0 {
+		service.Pid = c.Pid
+	}
+	if c.PidsLimit != 0 {
+		service.PidsLimit = int64(c.PidsLimit)
+	}
+
+	if len(c.Pid) > 0 {
+		service.Platform = c.Platform
+	}
+
 	if len(arguments["command"].ListValue()) > 0 {
 		service.Command = arguments["command"].ListValue()
 	}
